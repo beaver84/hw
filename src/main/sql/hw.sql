918c0e95d39c9abaec783b4f42b92c1dbbb215bb
KyungTae Bae <kyungtae@marvrus.com>
2022-09-20 17:48:10 +0900

    sql 수정

--- src/main/sql/hw.sql
+++ src/main/sql/hw.sql
@@ -9,39 +9,12 @@ CREATE TABLE `panalty` (
                            `panalty_seqno`	BIGINT UNSIGNED PRIMARY KEY NOT NULL,
                            `voc_seqno`	BIGINT UNSIGNED	NOT NULL,
                            `confirm_yn`	TinyInt(1)	NULL,
-                           `panalty_content`	VARCHAR(200)	NOT NULL	COMMENT '1:열림, 2:닫힘'
+                           `panalty_content`	VARCHAR(200)	NOT NULL
 );
 
 CREATE TABLE `compensation` (
                                 `compensation_seqno`	BIGINT UNSIGNED PRIMARY KEY  NOT NULL,
                                 `voc_seqno`	BIGINT UNSIGNED	NOT NULL,
-                                `compensation_content`	int	NOT NULL	COMMENT '1:열림, 2:닫힘'
+                                `compensation_content`	int	NOT NULL
 );
 
-ALTER TABLE `VOC` ADD CONSTRAINT `PK_VOC` PRIMARY KEY (
-                                                       `voc_seqno`
-    );
-
-ALTER TABLE `panalty` ADD CONSTRAINT `PK_PANALTY` PRIMARY KEY (
-                                                               `panalty_seqno`,
-                                                               `voc_seqno`
-    );
-
-ALTER TABLE `compensation` ADD CONSTRAINT `PK_COMPENSATION` PRIMARY KEY (
-                                                                         `compensation_seqno`,
-                                                                         `voc_seqno`
-    );
-
-ALTER TABLE `panalty` ADD CONSTRAINT `FK_VOC_TO_panalty_1` FOREIGN KEY (
-                                                                        `voc_seqno`
-    )
-    REFERENCES `VOC` (
-                      `voc_seqno`
-        );
-
-ALTER TABLE `compensation` ADD CONSTRAINT `FK_VOC_TO_compensation_1` FOREIGN KEY (
-                                                                                  `voc_seqno`
-    )
-    REFERENCES `VOC` (
-                      `voc_seqno`
-        );
